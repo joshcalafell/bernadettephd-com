18412801dec158d9d5456fbdba0cdad691fca4aa
Joshua Waggoner <joshuamichaelwaggoner@gmail.com>
2020-05-09 16:47:35 -0700

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bmc-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'bernadettephd-com';
+}
