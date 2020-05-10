4770ceabf2e0bfd9a9ff3f283e401720d12bc92a
Joshua Waggoner <joshuamichaelwaggoner@gmail.com>
2020-05-09 17:43:12 -0700

    Initial Commit :})

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,12 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { UniversalInterceptorService } from './interceptors/universal-interceptor.service';
 import { AppMaterialModule } from './material/app-material.module';
 import { PipesModule } from './pipes/pipes.module';
 
 @NgModule({
   imports: [CommonModule, AppMaterialModule, PipesModule],
-  providers: [UniversalInterceptorService],
+  providers: [],
   declarations: [],
   exports: [AppMaterialModule, PipesModule]
 })
